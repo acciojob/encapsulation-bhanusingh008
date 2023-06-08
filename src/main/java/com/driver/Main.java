250e5d462181a4c5cb5ea5e8caa51a894d399583
Bhanu Singh <122738012+bhanusingh008@users.noreply.github.com>
2023-06-08 12:54:24 +0530

    Update Main.java
    Created an object of RWOnly and used getter and setter to manipulate private variable in that class.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+
+        RWOnly rw = new RWOnly();
+
+        //rw.name = "bhanu";
+        /* does not work as variable is private
+        (only accessible in the class it is created in)..
+         Error: 'name' has private access in 'com.driver.RWOnly'
+         */
+
+        System.out.println(rw.getName());
+
+        rw.setName("Abhay");
+
+        System.out.println(rw.getName());
+
+    }
 }
